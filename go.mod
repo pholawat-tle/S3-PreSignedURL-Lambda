bf63b126883fcecd192e288117b70d5ee81ba0b3
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-07-21 17:16:20 +0700

    Implement Presigned URL Generation

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module s3-presigned
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.25.0
+require (
+	github.com/aws/aws-lambda-go v1.25.0
+	github.com/aws/aws-sdk-go-v2/config v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
 
 type Event struct {
-	ItemName string `json:"item-name"`
+	BucketName string `json:"bucket-name"`
+	ItemName   string `json:"item-name"`
 }
 
 type Response struct {
@@ -11,7 +19,23 @@ type Response struct {
 }
 
 func HandleLambdaEvent(event Event) (Response, error) {
-	return Response{}, nil
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
+	if err != nil {
+		return Response{}, err
+	}
+
+	params := s3.GetObjectInput{
+		Bucket: &event.BucketName,
+		Key:    &event.ItemName,
+	}
+	presignedClient := s3.NewPresignClient(s3.NewFromConfig(cfg), s3.WithPresignExpires(1*time.Minute))
+
+	object, err := presignedClient.PresignGetObject(context.TODO(), &params)
+	if err != nil {
+		return Response{}, err
+	}
+
+	return Response{PresignedURL: object.URL}, nil
 }
 
 func main() {
